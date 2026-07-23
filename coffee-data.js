5c5bbe3f9b7af7a489518093479a56dedfefdcb8
KlemenG0 <klemengradisnik0@gmail.com>
2026-07-23 15:56:04 +0200

    coffee-data.js

--- coffee-data.js
+++ coffee-data.js
@@ -3,7 +3,7 @@ const coffees = [
 {
 id:"espresso",
 name:"Espresso",
-category:"espresso",
+category:"Espresso",
 description:"Majhna koncentrirana kava z intenzivnim okusom.",
 origin:"Italija",
 preparation:"Približno 7–9 g mlete kave in 25–30 ml vode pri visokem tlaku."
@@ -13,7 +13,7 @@ preparation:"Približno 7–9 g mlete kave in 25–30 ml vode pri visokem tlaku.
 {
 id:"cappuccino",
 name:"Cappuccino",
-category:"milk",
+category:"Espresso + mleko",
 description:"Espresso z vročim mlekom in mlečno peno.",
 origin:"Italija",
 preparation:"Običajno razmerje 1/3 espresso, 1/3 mleko, 1/3 pena."
@@ -23,7 +23,7 @@ preparation:"Običajno razmerje 1/3 espresso, 1/3 mleko, 1/3 pena."
 {
 id:"americano",
 name:"Americano",
-category:"methods",
+category:"Načini priprave",
 description:"Espresso razredčen z vročo vodo.",
 origin:"ZDA",
 preparation:"Espresso se prelije z dodatkom vroče vode."
