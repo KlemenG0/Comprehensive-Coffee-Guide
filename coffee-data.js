bd28624a612c820c3a0667b7f01875d0b972bee0
KlemenG0 <klemengradisnik0@gmail.com>
2026-07-23 15:36:12 +0200

    coffee-data.js

--- coffee-data.js
+++ coffee-data.js
@@ -1,226 +1,32 @@
 const coffees = [
 
 {
-    id: "espresso",
-    name: "Espresso",
-    category: "espresso",
-    image: "espresso.jpg",
-
-    definition:
-    "Klasična italijanska kratka kava z intenzivnim okusom.",
-
-    origin:
-    "Italija",
-
-    composition:
-    "7–9 g fino mlete kave",
-
-    preparation:
-    "Ekstrakcija pod visokim tlakom skozi espresso aparat.",
-
-    characteristics:
-    "Majhna količina, gosta krema, intenzivna aroma.",
-
-    serving:
-    "Majhna skodelica za espresso",
-
-    strength:
-    "Močna",
-
-    caffeine:
-    "≈ 60–80 mg"
+id:"espresso",
+name:"Espresso",
+category:"espresso",
+description:"Majhna koncentrirana kava z intenzivnim okusom.",
+origin:"Italija",
+preparation:"Približno 7–9 g mlete kave in 25–30 ml vode pri visokem tlaku."
 },
 
 
 {
-    id: "cafe-latte",
-    name: "Cafè latte",
-    category: "espresso-mleko",
-    image: "cafe-latte.jpg",
-
-    definition:
-    "Espresso z večjo količino vročega mleka.",
-
-    origin:
-    "Italija",
-
-    composition:
-    "1 espresso + mleko",
-
-    preparation:
-    "Espresso se dopolni s segretim mlekom in tanko plastjo pene.",
-
-    characteristics:
-    "Nežen okus, kremasta tekstura.",
-
-    serving:
-    "Večji kozarec ali skodelica",
-
-    strength:
-    "Blaga",
-
-    caffeine:
-    "≈ 60–80 mg"
+id:"cappuccino",
+name:"Cappuccino",
+category:"milk",
+description:"Espresso z vročim mlekom in mlečno peno.",
+origin:"Italija",
+preparation:"Običajno razmerje 1/3 espresso, 1/3 mleko, 1/3 pena."
 },
 
 
 {
-    id: "filter-kava",
-    name: "Filter kava",
-    category: "priprava",
-    image: "filter-kava.jpg",
-
-    definition:
-    "Kava pripravljena s počasnim prehajanjem vode skozi mleto kavo.",
-
-    origin:
-    "Različne regije sveta",
-
-    composition:
-    "Mleta kava + vroča voda",
-
-    preparation:
-    "Voda ekstrahira aromatične spojine skozi filter.",
-
-    characteristics:
-    "Čist okus, poudarjene arome.",
-
-    serving:
-    "Večja skodelica",
-
-    strength:
-    "Srednja",
-
-    caffeine:
-    "≈ 80–120 mg"
-},
-
-
-{
-    id: "turska-kava",
-    name: "Turška kava",
-    category: "regionalno",
-    image: "turska-kava.jpg",
-
-    definition:
-    "Tradicionalna kava pripravljena v džezvi.",
-
-    origin:
-    "Bližnji vzhod in Balkan",
-
-    composition:
-    "Zelo fino mleta kava + voda",
-
-    preparation:
-    "Kava se skuha skupaj z vodo brez filtriranja.",
-
-    characteristics:
-    "Močan okus in usedlina na dnu skodelice.",
-
-    serving:
-    "Majhna skodelica",
-
-    strength:
-    "Močna",
-
-    caffeine:
-    "≈ 50–70 mg"
-},
-
-
-{
-    id: "ledena-kava",
-    name: "Ledena kava",
-    category: "hladno",
-    image: "ledena-kava.jpg",
-
-    definition:
-    "Hladna različica kavnega napitka.",
-
-    origin:
-    "Sodobne različice po svetu",
-
-    composition:
-    "Kava, led, mleko ali dodatki",
-
-    preparation:
-    "Ohlajena kava postrežena z ledom.",
-
-    characteristics:
-    "Osvežilna in manj intenzivna.",
-
-    serving:
-    "Visok kozarec",
-
-    strength:
-    "Odvisna od priprave",
-
-    caffeine:
-    "Odvisno od količine kave"
-},
-
-
-{
-    id: "affogato",
-    name: "Affogato",
-    category: "sladice",
-    image: "affogato.jpg",
-
-    definition:
-    "Italijanska sladica iz sladoleda in espressa.",
-
-    origin:
-    "Italija",
-
-    composition:
-    "Vaniljev sladoled + espresso",
-
-    preparation:
-    "Vroč espresso se prelije preko sladoleda.",
-
-    characteristics:
-    "Kombinacija grenkega in sladkega.",
-
-    serving:
-    "Majhna skodelica",
-
-    strength:
-    "Srednja",
-
-    caffeine:
-    "≈ 60–80 mg"
-},
-
-
-{
-    id: "irska-kava",
-    name: "Irska kava",
-    category: "zganje",
-    image: "irska-kava.jpg",
-
-    definition:
-    "Kavni napitek z dodatkom irskega viskija.",
-
-    origin:
-    "Irska",
-
-    composition:
-    "Kava, viski, sladkor, smetana",
-
-    preparation:
-    "Sladka kava z alkoholom in plastjo smetane.",
-
-    characteristics:
-    "Topel, aromatičen in sladek napitek.",
-
-    serving:
-    "Steklen kozarec",
-
-    strength:
-    "Srednja",
-
-    caffeine:
-    "≈ 60–80 mg"
+id:"americano",
+name:"Americano",
+category:"methods",
+description:"Espresso razredčen z vročo vodo.",
+origin:"ZDA",
+preparation:"Espresso se prelije z dodatkom vroče vode."
 }
 
 ];
