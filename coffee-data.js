1d08081e6209f23913d7f7b2023a4abe77a08cf7
KlemenG0 <klemengradisnik0@gmail.com>
2026-07-23 14:47:37 +0200

    coffee-data.js

--- coffee-data.js
+++ coffee-data.js
@@ -1,44 +1,226 @@
 const coffees = [
+
 {
     id: "espresso",
-
     name: "Espresso",
-
     category: "espresso",
-
     image: "espresso.jpg",
 
     definition:
-    "Espresso je majhen, koncentriran kavni napitek, pripravljen s prepuščanjem vroče vode pod visokim tlakom skozi fino mleto kavo.",
-
+    "Klasična italijanska kratka kava z intenzivnim okusom.",
 
     origin:
-    "Italija. Razvil se je v začetku 20. stoletja z razvojem prvih espresso aparatov.",
-
+    "Italija",
 
     composition:
-    {
-        coffee: "7–9 g mlete kave",
-        water: "25–30 ml",
-        milk: "Brez mleka"
-    },
-
+    "7–9 g fino mlete kave",
 
     preparation:
-    "Kava se pripravi z ekstrakcijo pri približno 9 bar tlak in temperaturi vode okoli 90–96 °C.",
+    "Ekstrakcija pod visokim tlakom skozi espresso aparat.",
 
+    characteristics:
+    "Majhna količina, gosta krema, intenzivna aroma.",
 
     serving:
-    "Majhna skodelica (25–30 ml)",
+    "Majhna skodelica za espresso",
 
-
-    strength: 5,
-
+    strength:
+    "Močna",
 
     caffeine:
-    "Približno 60–80 mg kofeina",
-  
+    "≈ 60–80 mg"
+},
 
+
+{
+    id: "cafe-latte",
+    name: "Cafè latte",
+    category: "espresso-mleko",
+    image: "cafe-latte.jpg",
+
+    definition:
+    "Espresso z večjo količino vročega mleka.",
+
+    origin:
+    "Italija",
+
+    composition:
+    "1 espresso + mleko",
+
+    preparation:
+    "Espresso se dopolni s segretim mlekom in tanko plastjo pene.",
+
+    characteristics:
+    "Nežen okus, kremasta tekstura.",
+
+    serving:
+    "Večji kozarec ali skodelica",
+
+    strength:
+    "Blaga",
+
+    caffeine:
+    "≈ 60–80 mg"
+},
+
+
+{
+    id: "filter-kava",
+    name: "Filter kava",
+    category: "priprava",
+    image: "filter-kava.jpg",
+
+    definition:
+    "Kava pripravljena s počasnim prehajanjem vode skozi mleto kavo.",
+
+    origin:
+    "Različne regije sveta",
+
+    composition:
+    "Mleta kava + vroča voda",
+
+    preparation:
+    "Voda ekstrahira aromatične spojine skozi filter.",
+
+    characteristics:
+    "Čist okus, poudarjene arome.",
+
+    serving:
+    "Večja skodelica",
+
+    strength:
+    "Srednja",
+
+    caffeine:
+    "≈ 80–120 mg"
+},
+
+
+{
+    id: "turska-kava",
+    name: "Turška kava",
+    category: "regionalno",
+    image: "turska-kava.jpg",
+
+    definition:
+    "Tradicionalna kava pripravljena v džezvi.",
+
+    origin:
+    "Bližnji vzhod in Balkan",
+
+    composition:
+    "Zelo fino mleta kava + voda",
+
+    preparation:
+    "Kava se skuha skupaj z vodo brez filtriranja.",
+
+    characteristics:
+    "Močan okus in usedlina na dnu skodelice.",
+
+    serving:
+    "Majhna skodelica",
+
+    strength:
+    "Močna",
+
+    caffeine:
+    "≈ 50–70 mg"
+},
+
+
+{
+    id: "ledena-kava",
+    name: "Ledena kava",
+    category: "hladno",
+    image: "ledena-kava.jpg",
+
+    definition:
+    "Hladna različica kavnega napitka.",
+
+    origin:
+    "Sodobne različice po svetu",
+
+    composition:
+    "Kava, led, mleko ali dodatki",
+
+    preparation:
+    "Ohlajena kava postrežena z ledom.",
+
+    characteristics:
+    "Osvežilna in manj intenzivna.",
+
+    serving:
+    "Visok kozarec",
+
+    strength:
+    "Odvisna od priprave",
+
+    caffeine:
+    "Odvisno od količine kave"
+},
+
+
+{
+    id: "affogato",
+    name: "Affogato",
+    category: "sladice",
+    image: "affogato.jpg",
+
+    definition:
+    "Italijanska sladica iz sladoleda in espressa.",
+
+    origin:
+    "Italija",
+
+    composition:
+    "Vaniljev sladoled + espresso",
+
+    preparation:
+    "Vroč espresso se prelije preko sladoleda.",
+
+    characteristics:
+    "Kombinacija grenkega in sladkega.",
+
+    serving:
+    "Majhna skodelica",
+
+    strength:
+    "Srednja",
+
+    caffeine:
+    "≈ 60–80 mg"
+},
+
+
+{
+    id: "irska-kava",
+    name: "Irska kava",
+    category: "zganje",
+    image: "irska-kava.jpg",
+
+    definition:
+    "Kavni napitek z dodatkom irskega viskija.",
+
+    origin:
+    "Irska",
+
+    composition:
+    "Kava, viski, sladkor, smetana",
+
+    preparation:
+    "Sladka kava z alkoholom in plastjo smetane.",
+
+    characteristics:
+    "Topel, aromatičen in sladek napitek.",
+
+    serving:
+    "Steklen kozarec",
+
+    strength:
+    "Srednja",
+
+    caffeine:
+    "≈ 60–80 mg"
 }
 
 ];
