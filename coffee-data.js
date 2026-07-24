73cc423fb61cf57e1506bc9660e0a9fbd72b677f
KlemenG0 <klemengradisnik0@gmail.com>
2026-07-24 12:34:18 +0200

    coffee-data.js

--- coffee-data.js
+++ coffee-data.js
@@ -5,7 +5,7 @@ const coffees = [
     name: "Espresso",
     category: "espresso",
     categoryName: "Espresso",
-    image: images/"espresso.png",
+    image: "images/espresso.png",
     description: "Majhen koncentriran kavni napitek z bogato kremo.",
     preparation: "Pripravljen z ekstrakcijo približno 30 ml vode pod tlakom skozi fino mleto kavo.",
     caffeine: 5,
