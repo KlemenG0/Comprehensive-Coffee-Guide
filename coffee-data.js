b33fd92e1095cbff124a7177da526c7d44c84b66
KlemenG0 <klemengradisnik0@gmail.com>
2026-08-03 18:05:14 +0200

    coffee-data.js

--- coffee-data.js
+++ coffee-data.js
@@ -13,6 +13,18 @@ const coffees = [
 },
 
 {
+    id: "ristretto",
+    name: "Ristretto",
+    category: "espresso",
+    cardImage: "images/ristretto.jpg",
+    detailImage: "images/ristretto_1.jpg",
+    description: "Majhen koncentriran kavni napitek z bogato kremo.",
+    preparation: "Pripravljen z ekstrakcijo približno 30 ml vode pod tlakom skozi fino mleto kavo.",
+    caffeine: 5,
+    origin: "Italija"
+},
+    
+{
     id: "cappuccino",
     name: "Cappuccino",
     category: "milk",
@@ -22,6 +34,6 @@ const coffees = [
     preparation: "Pripravi se iz espressa, vročega mleka in goste mlečne pene.",
     caffeine: 4,
     origin: "Italija"
-}
-
+},
+    
 ];
