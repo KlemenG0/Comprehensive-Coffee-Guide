2f8f531562817fee458aa3878b088012e7eda405
KlemenG0 <klemengradisnik0@gmail.com>
2026-08-04 10:55:48 +0200

    coffee-data.js

--- coffee-data.js
+++ coffee-data.js
@@ -6,8 +6,8 @@ const coffees = [
     category: "espresso",
     cardImage: "images/espresso.png",
     detailImage: "images/espresso_1.jpg",
-    description: "Majhen koncentriran, sirupasti kavni napitek z bogato kremo. Razmerje med fino mleto kavo in vodo je 1:2. Običajna količina napitka znaša med 25 mL in 30 mL, količina samega kofeina pa 64 mg. Napitek se postreže vroč, pogosto v majhni skodelici.",
-    preparation: "Pripravljen z ekstrakcijo fino mlete kave z vročo vodo (do 96 °C) pri relativnem tlaku cca. 9 bar. Čas ekstrakcije je nekoliko krajši od 30 sekund.",
+    description: "Majhen koncentriran, sirupasti kavni napitek z bogato kremo. Razmerje med fino mletimi kavnimi zrni in vodo je 1:2. Običajna količina napitka znaša med 25 mL in 30 mL, količina samega kofeina pa 64 mg. Napitek se postreže vroč, pogosto v majhni skodelici.",
+    preparation: "Pripravljen z ekstrakcijo fino mletih kavnih zrn z vročo vodo (do 96 °C) pri relativnem tlaku cca. 9 bar. Čas ekstrakcije je nekoliko krajši od 30 sekund.",
     caffeine: 5,
     alternativeNames: "Un caffè (Italija), Café solo (Španija), Bica (Lizbona, Portugalska), Cimbalino (Porto, Portugalska), Café express (Francija).",
     origin: "Turin in Milan, Italija. Konec 19. in začetek 20. stoletja."
