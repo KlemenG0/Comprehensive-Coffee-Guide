df995742f6df2899088b05703ab02d301c2df97d
KlemenG0 <klemengradisnik0@gmail.com>
2026-08-04 08:49:35 +0200

    coffee-data.js

--- coffee-data.js
+++ coffee-data.js
@@ -9,7 +9,7 @@ const coffees = [
     description: "Majhen koncentriran, sirupasti kavni napitek z bogato kremo. Razmerje med fino mleto kavo in vodo je 1:2. Običajna količina napitka znaša 25 mL - 30 mL, količina kofeina pa 64 mg. Napitek se postreže vroč, pogosto v majhni skodelici.",
     preparation: "Pripravljen z ekstrakcijo fino mlete kave z vročo vodo (do 96 °C) pri relativnem tlaku cca. 9 bar.",
     caffeine: 5,
-    alternativeName: "Un caffè (Italija), Café solo (Španija), Bica (Lizbona, Portugalska), Cimbalino (Porto, Portugalska), Café express (Francija).
+    alternativeName: "Un caffè (Italija), Café solo (Španija), Bica (Lizbona, Portugalska), Cimbalino (Porto, Portugalska), Café express (Francija).",
     origin: "Turin in Milan, Italija. Konec 19. in začetek 20. stoletja."
 },
 
